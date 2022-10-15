0e6e68ec89213511ddc8aaa122e3e4621ff49b62
johnescamargo <johnescamargo@gmail.com>
2022-10-15 20:00:33 -0300

    Basic auth

---
+++ src/main/java/com/example/demosecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.demosecurity.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+	}
+}
