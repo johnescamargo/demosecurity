e954d572a81cbe43781e32ee50dfafa99c53602b
johnescamargo <johnescamargo@gmail.com>
2022-10-18 21:26:46 -0300

    Role based authentication

--- src/main/java/com/example/demosecurity/controller/StudentController.java
+++ src/main/java/com/example/demosecurity/controller/StudentController.java
@@ -14,13 +14,20 @@ import com.example.demosecurity.model.Student;
 @RequestMapping("api/v1/students")
 public class StudentController {
 
-	private static final List<Student> STUDENTS = Arrays.asList(new Student(1, "James Bond"),
-			new Student(2, "Maria Jones"), new Student(3, "Anna Smith"));
+	private static final List<Student> STUDENTS = Arrays.asList(
+			new Student(1, "James Bond"),
+			new Student(2, "Maria Jones"), 
+			new Student(3, "Anna Smith")
+			);
 
 	@GetMapping(path = "{studentId}")
 	public Student getStudent(@PathVariable("studentId") Integer studentId) {
-		return STUDENTS.stream().filter(student -> studentId.equals(student.getStudentId())).findFirst()
-				.orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exists"));
+		return STUDENTS.stream()
+				.filter(student -> studentId.equals(student.getStudentId()))
+				.findFirst()
+				.orElseThrow(() -> new IllegalStateException(
+						"Student " + studentId + " does not exists")
+				);
 	}
 
 }
